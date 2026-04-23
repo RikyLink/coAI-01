7c94741a10c472168bbe1fe84bd1b52fc2adffd6
RikyLink <dyonhar@gmail.com>
2026-04-22 23:54:36 -0300

    adicionado as funções basicas

---
+++ background.js
@@ -0,0 +1,4 @@
+// Permite que o painel lateral abra ao clicar no ícone da extensão
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
